100bcc0027b40da8757ca91240b6ee5886ed8b00
Soham041201 <sattigeri.soham@gmail.com>
2022-06-06 10:53:16 +0530

    delete added in cors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
   cors: {
     origin: "*",
-    methods: ["GET", "POST"],
+    methods: ["GET", "POST","PUT","DELETE"],
   },
 });
 
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: "*",
-    methods: ["GET", "POST"],
+    methods: ["GET", "POST","PUT","DELETE"],
   })
 );
 
