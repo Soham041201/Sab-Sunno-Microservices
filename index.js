ad959ab02faf583f7c172cc3fc2bb7b9066886f3
Soham041201 <sattigeri.soham@gmail.com>
2022-06-06 11:22:42 +0530

    Room delete functionality  added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
   cors: {
     origin: "*",
-    methods: ["GET", "POST","PUT","DELETE"],
+    methods: ["GET", "POST"],
   },
 });
 
