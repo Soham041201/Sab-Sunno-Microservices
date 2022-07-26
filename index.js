34b570f0546710cc8d17af1d99f2a36dc79224f4
Soham041201 <sattigeri.soham@gmail.com>
2022-07-26 16:54:34 +0530

    Chat server created

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 const { ObjectId } = require("mongodb");
 require("dotenv").config();
 const server = require("http").createServer(app);
+const chatServer = require("./chatServer");
 
 const io = require("socket.io")(server, {
   cors: {
