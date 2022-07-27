f96664b59aa147dbc8b98fda23a7cbcaea65eb66
Soham041201 <sattigeri.soham@gmail.com>
2022-07-27 14:47:12 +0530

    Added condition

--- SunnoChat/chatServer.js
+++ SunnoChat/chatServer.js
@@ -4,10 +4,10 @@ users= []
 function chatServer(socket,io){
     socket.on('chat-join',(data)=>{
         console.log(data + socket.id);
-        users.push(socket.id);
+        !users.includes(socket.id) && users.push(socket.id);
         console.log(users);
         users.map((user)=>{
-            if(user != socket.id && !user.contains(socket.id)){
+            if(user != socket.id){
                 io.to(user).emit('chat-connected',{
                     connectedUser:socket.id,
                     message: "connected"
