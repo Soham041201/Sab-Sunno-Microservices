fc604fdc9efff6ffc515703290c843d7ddbd80f5
Soham041201 <sattigeri.soham@gmail.com>
2022-07-27 18:53:29 +0530

    Message send

--- SunnoChat/chatServer.js
+++ SunnoChat/chatServer.js
@@ -7,9 +7,13 @@ function chatServer(socket, io) {
     console.log("Clients connected", clients);
     clients.map((user) => {
       if (user != socket.id) {
-        io.in('chat-room').emit("chat-connected", JSON.stringify({
+        io.to(user).emit("chat-connected", JSON.stringify({
           connectedUser: socket.id,
           message: "connected",
+        }));
+        socket.emit("chat-connected", JSON.stringify({
+            connectedUser: user,
+            message: "connected",
         }));
       }
     });
