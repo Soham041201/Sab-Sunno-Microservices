d4f4c6dbd82fc94698874d5392c68c408a22676f
Soham041201 <sattigeri.soham@gmail.com>
2022-07-27 14:42:59 +0530

    Added condition

--- SunnoChat/chatServer.js
+++ SunnoChat/chatServer.js
@@ -1,7 +1,7 @@
 
 users= []
 
-function chatServer(socket,io,users){
+function chatServer(socket,io){
     socket.on('chat-join',(data)=>{
         console.log(data + socket.id);
         users.push(socket.id);
