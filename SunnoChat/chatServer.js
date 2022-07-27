d3fd20ab9c6a88123ec84dabced91b398c8f2fdd
Soham041201 <sattigeri.soham@gmail.com>
2022-07-27 16:34:16 +0530

    Chat connection

--- SunnoChat/chatServer.js
+++ SunnoChat/chatServer.js
@@ -1,27 +1,33 @@
+function chatServer(socket, io) {
+  const clients = Array.from(io.sockets.adapter.rooms.get("chat-room") || []);
 
-
-
-function chatServer(socket,io){
-    const clients = Array.from(io.sockets.adapter.rooms.get('chat-room') || []);
-    
-    socket.on('chat-join',(data)=>{
-        console.log(data + socket.id);
-        socket.join('chat-room');
-        console.log("Clients connected", clients);
-        clients.map((user)=>{
-            if(user != socket.id){
-                io.to(user).emit('chat-connected',{
-                    connectedUser:socket.id,
-                    message: "connected"
-                });
-            }
-        })
+  socket.on("chat-join", (data) => {
+    console.log(data + socket.id);
+    socket.join("chat-room");
+    console.log("Clients connected", clients);
+    clients.map((user) => {
+      if (user != socket.id) {
+        io.to(user).emit("chat-connected", {
+          connectedUser: socket.id,
+          message: "connected",
+        });
+      }
     });
+  });
 
-    socket.on("disconnect", () => {
-        console.log("============Socket disconnected=============", socket.id);
-        socket.leave('chat-room');
+  socket.on("send-message", (data) => {
+    console.log(data);
+    io.to("chat-room").emit("receive-message", {
+      message: data,
+      sender: socket.id,
     });
+  })
+
+
+  socket.on("disconnect", () => {
+    console.log("============Socket disconnected=============", socket.id);
+    socket.leave("chat-room");
+  });
 }
 
 module.exports = chatServer;
