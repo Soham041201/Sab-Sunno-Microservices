0b18bd401e3049500a8e15d9af35f001e1f1bff1
Soham041201 <sattigeri.soham@gmail.com>
2022-07-27 11:17:37 +0530

    User data

--- SunnoChat/chatServer.js
+++ SunnoChat/chatServer.js
@@ -1,7 +1,10 @@
+const users = []
 
 function chatServer(socket,io){
     socket.on('chat-join',(data)=>{
         console.log(data + socket.id);
+        users.push(socket.id);
+        console.log(users);
     });
 }
 
