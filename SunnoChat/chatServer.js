66975ce9c79fa7a1cd899a24212973b78c9fc8b4
Soham041201 <sattigeri.soham@gmail.com>
2022-09-04 13:44:52 +0530

    Socket data

--- SunnoChat/chatServer.js
+++ SunnoChat/chatServer.js
@@ -33,7 +33,7 @@ function chatServer(socket, io) {
   socket.on('is_online',(data)=>{
     console.log("===============is_online SOCKET EVENT==========");
     console.log("=======online socket========",data);
-    socket.broadcast.emit('user_online',Array.from(io.sockets.adapter.rooms.get("chat-room")));
+    // socket.broadcast.emit('user_online',Array.from(io.sockets.adapter.rooms.get("chat-room")));
   })
 
   socket.on('last_seen',()=>{})
