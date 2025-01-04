e78010a0cd64db30f905324e12d145d763dd813d
Soham041201 <sattigeri.soham@gmail.com>
2025-01-04 14:47:18 +0530

    feat: move to env

--- api-service/openai.js
+++ api-service/openai.js
@@ -1,11 +1,14 @@
+const env = require('dotenv').config();
+
 const MODEL = 'gpt-4o-mini-realtime-preview-2024-12-17';
 const VOICE = 'verse';
 
 const fetchEphimerialToken = async () => {
+
   const r = await fetch('https://api.openai.com/v1/realtime/sessions', {
     method: 'POST',
     headers: {
-      Authorization: `Bearer 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`,
+      Authorization: `Bearer ${env.parsed.OPENAI_API_KEY}`,
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
